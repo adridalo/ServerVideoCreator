17d45245cfe4d294501da58936f351b74b14385a
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-07-09 08:17:15 -0400

    Update batch script and workflow file

--- publish.bat
+++ publish.bat
@@ -1,13 +1,8 @@
 @echo off
-REM Make sure we're in the script directory
 cd /d %~dp0
+set PYTHONPATH=%CD%
 
-REM Show current directory
-echo Building in %CD%
-
-REM Compile with explicit path
 pyinstaller --noconfirm --onefile --name ServerVideoCreator --paths=%CD% main.py
 
-REM Copy additional files
 copy ffmpeg.exe dist\
 copy proxy.config.json dist\
