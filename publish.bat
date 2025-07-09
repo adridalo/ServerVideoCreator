0655cde38b1d84d0ba2399181763d99242f0e23f
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-07-09 08:23:08 -0400

    ...

--- publish.bat
+++ publish.bat
@@ -1,4 +1,6 @@
 @echo off
+REM Make sure we are in the directory where publish.bat lives
+cd /d %~dp0
 
 set PYTHONPATH=%CD%
 pyinstaller --noconfirm --onefile --name ServerVideoCreator --paths=%CD% main.py
