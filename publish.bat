0f230310cd2aed3f1d0dc4a084f532e0294d7011
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-07-09 08:20:16 -0400

    ...

--- publish.bat
+++ publish.bat
@@ -1,8 +1,6 @@
 @echo off
-cd /d %~dp0
+
 set PYTHONPATH=%CD%
-
 pyinstaller --noconfirm --onefile --name ServerVideoCreator --paths=%CD% main.py
-
 copy ffmpeg.exe dist\
 copy proxy.config.json dist\
