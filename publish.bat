5e193d639a8f06c4d1bcfaa0bca42de64b8be7d7
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-07-09 08:03:08 -0400

    Use spec file for generating exe

--- publish.bat
+++ publish.bat
@@ -2,7 +2,4 @@
 REM Force script to run from script's directory
 cd /d %~dp0
 
-pyinstaller --noconfirm --onefile --name ServerVideoCreator --paths=. main.py
-
-copy ffmpeg.exe dist\
-copy proxy.config.json dist\
+pyinstaller ServerVideoCreator.spec
