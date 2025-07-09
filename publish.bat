41b3d988ed93e44a962b8358550057a0076007a2
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-07-09 07:56:05 -0400

    Add step to change to project dir

--- publish.bat
+++ publish.bat
@@ -1,3 +1,8 @@
-pyinstaller --noconfirm --onefile --name ServerVideoCreator main.py
+@echo off
+REM Force script to run from script's directory
+cd /d %~dp0
+
+pyinstaller --noconfirm --onefile --name ServerVideoCreator --paths=. main.py
+
 copy ffmpeg.exe dist\
 copy proxy.config.json dist\
