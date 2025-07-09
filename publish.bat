3768bcd720ad75101afbf307fe5e5ba7a02536b7
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-07-09 08:07:09 -0400

    ...

--- publish.bat
+++ publish.bat
@@ -1,5 +1,13 @@
 @echo off
-REM Force script to run from script's directory
+REM Make sure we're in the script directory
 cd /d %~dp0
 
-pyinstaller ServerVideoCreator.spec
+REM Show current directory
+echo Building in %CD%
+
+REM Compile with explicit path
+pyinstaller --noconfirm --onefile --name ServerVideoCreator --paths=%CD% main.py
+
+REM Copy additional files
+copy ffmpeg.exe dist\
+copy proxy.config.json dist\
