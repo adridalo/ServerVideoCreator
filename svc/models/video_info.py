d719f08f523ee9eb6da94f9ca43b06469417214c
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-07-14 10:37:09 -0400

    Create model for downloaded video info

---
+++ svc/models/video_info.py
@@ -0,0 +1,49 @@
+import math
+import ffmpeg
+
+
+class DownloadedVideoInfo:
+    def __init__(self, path, resolution, fps):
+        self.path = path
+        self.resolution = resolution
+        self.fps = fps
+
+    def _get_pretty_resolution(resolution_height, frame_rate):
+        rounded_fps = math.ceil(frame_rate)
+
+        if resolution_height == 1080:
+            return f"HD{rounded_fps}"
+        elif resolution_height == 3840 or resolution_height == 4096:
+            return f"4K{rounded_fps}"
+        else:
+            return f"{resolution_height}p"
+
+    def get_video_info(video):
+        try:
+            probe = ffmpeg.probe(video)
+
+            video_stream = next(
+                (stream for stream in probe["streams"] if stream["codec_type"] == "video"),
+                None
+            )
+
+            if video_stream is None:
+                raise ValueError("No video stream found")
+            
+            height = video_stream['height']
+            width = video_stream['width']
+
+            rate = video_stream.get("avg_frame_rate", "0/0")
+            num, denom = map(int, rate.split("/"))
+            frame_rate = math.ceil(num / denom) if denom != 0 else 0
+
+            return DownloadedVideoInfo(
+                video,
+                (height, width),
+                frame_rate
+            )
+
+        except ffmpeg.Error as e:
+            print("FFmpeg error:", e.stderr.decode())
+        except Exception as e:
+            print("Error:", str(e))
